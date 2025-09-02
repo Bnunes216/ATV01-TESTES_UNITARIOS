8b25f03a66d26480eaef8cd4749253470cb1fd0e
João Marcelo Teixeira de Almeida <joaomarcello04@gmail.com>
2025-09-02 20:34:55 -0300

    teste 01

---
+++ testeng/src/main/java/asshole/Calculator.java
@@ -0,0 +1,5 @@
+package asshole;
+
+public class Calculator {
+    
+}
---
+++ testeng/src/test/java/CalculatorTest.java
@@ -0,0 +1,20 @@
+/* 
+package asshole;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+
+public class CalculatorTest{
+
+    @Test
+    public void testSoma() {
+        int resultado = 2 + 2;
+        assertEquals(resultado, 4, "A soma deve ser 4");
+    }
+
+    @Test
+    public void testMultiplicacao() {
+        int resultado = 2 * 3;
+        assertEquals(resultado, 6, "A multiplicação deve ser 6");
+    }
+} */
